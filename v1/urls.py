68d0f6cb74749614a07da8acd5b8b78e4756c5df
pedvel <pedvel@gmail.com>
2024-09-26 22:09:24 -0300

    redirections and code cleaning

--- v1/urls.py
+++ v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import CustomLoginView, ContentUploadView, snap, home
+from .views import CustomLoginView, snap, home
 
 
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('snap/', snap, name='snap'),
-    path('api/upload/', ContentUploadView.as_view(), name='content-upload'),
     path('home/', home, name='home'),
     path('explore/', views.explore, name='explore'),
     path('profile/', views.profile, name='profile'),
